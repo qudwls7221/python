3053469a5d2ffb3c34cb2da210593061f3822cff
qudwls7221 <qudwls7221@gmail.com>
2022-03-19 01:24:43 +0900

    variable,arithmetic

---
+++ 220319_variable.py
@@ -0,0 +1,11 @@
+#변수 선언
+myMoney = 0
+
+#변수 호출
+print (myMoney)
+
+#변수 데이터 변경
+myMoney = 1500
+
+#변수 호출
+print (myMoney)
---
+++ 220319_variable_2.py
@@ -0,0 +1,7 @@
+#변수선언과 사칙연산
+firstNum = 100
+secondNum = 200
+print ("firstNum + secondNum",firstNum + secondNum)
+print ("firstNum - secondNum",firstNum - secondNum)
+print ("sirstNum * secondNum",firstNum * secondNum)
+print ("firstNum / secondNum",firstNum / secondNum)
