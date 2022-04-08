67277508a9b3a460bdef816de10d071513fe9096
qudwls7221 <qudwls7221@gmail.com>
2022-04-08 15:47:44 +0900

    home

---
+++ BAEKJOON/220408.py
@@ -0,0 +1,14 @@
+A = input()
+B = input()
+
+su1 = (int(A) * int(B[2]))
+su2 = (int(A) * int(B[1]))
+su3 = (int(A) * int(B[0]))
+
+print (su1)
+print (su2)
+print (su3)
+
+end = (su1 + (su2*10) +(su3*100))
+
+print (end)
